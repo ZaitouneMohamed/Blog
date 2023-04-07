3d6306b3d19ae6868cf949882114f1858a267c28
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-04-07 21:44:55 +0000

    admin tag management

--- app/Http/Livewire/TagsList.php
+++ app/Http/Livewire/TagsList.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 
 class TagsList extends Component
 {
+    public $name;
     public function render()
     {
         return view('livewire.tags-list');
@@ -17,9 +18,23 @@ class TagsList extends Component
         return Tags::all();
     }
 
+    public function add()
+    {
+        $this->validate();
+        Tags::create([
+            "name" => $this->name
+        ]);
+        $this->getTagsProperty();
+        $this->name = "";
+    }
+
     public function delete($id)
     {
         Tags::find($id)->delete();
         $this->getTagsProperty();
     }
+
+    protected $rules = [
+        'name' => 'required',
+    ];
 }
