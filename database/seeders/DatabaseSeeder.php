c9d2d482d6791b5688ef748ae0c05b18763fe9d2
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-04-08 17:17:17 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(rolesSeeder::class);
         $this->call(UsersSeeder::class);
-        \App\Models\Tags::factory(10)->create();
-        \App\Models\Categorie::factory(10)->create();
-        \App\Models\Posts::factory(50)->create();
-        \App\Models\Comments::factory(50)->create();
+        // \App\Models\Tags::factory(10)->create();
+        // \App\Models\Categorie::factory(10)->create();
+        // \App\Models\Posts::factory(50)->create();
+        // \App\Models\Comments::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
