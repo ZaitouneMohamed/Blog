b6b1d6fd6cb78e0e1a8949f1811a767ec4d21327
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-05-24 20:58:11 +0100

    conversations and chat model & migration

---
+++ app/Models/Chat.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'sender_id',
+        'conversation_id',
+        'receiver_id',
+        'message',
+        'statue'
+    ];
+
+    // relations 
+
+    public function Convertations()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+    public function User()
+    {
+        return $this->belongsTo(User::class , 'sender_id');
+    }
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->diffForHumans();
+    }
+}
