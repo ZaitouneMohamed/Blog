8a01d9efcb6831f9d4283011159eeb7975d9833e
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-05-26 21:03:21 +0100

    small user changes in structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('image')->nullable();
+            $table->string('role')->nullable();
+            $table->string('description')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
