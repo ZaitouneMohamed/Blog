050f449a642c79957a4a7cc537619baa00b33bd3
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-06-06 18:04:13 +0100

    small changes

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.master.master')
 
 @section('content')
-    <section style="background-color: #eee;">
+    {{-- <section style="background-color: #eee;">
         <div class="container py-5">
             <div class="row">
                 <div class="col">
@@ -170,6 +170,98 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </section> --}}
+    <section style="background-color: #eee;">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">User</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <livewire:user.profile.picture />
+
+                    <div class="card mb-4 mb-lg-0">
+                        <livewire:user.profile.links />
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <livewire:user.profile.info />
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card mb-4 mb-md-0">
+                                <div class="card-body">
+                                    <p class="mb-4"><span class="text-primary font-italic me-1">roles
+                                            ({{ $user->roles->count() }}) </span>
+                                    </p>
+                                    <div>
+                                        <form action="{{ route('admin.AssignRoleToUser', ['id' => $user->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            <select class="form-select" name="role" aria-label="Default select example">
+                                                @foreach ($roles as $item)
+                                                    <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            <button type="submit" class="btn btn-success">assign role</button>
+                                        </form>
+                                    </div>
+                                    @foreach ($user->roles as $item)
+                                        <div class="d-flex">
+                                            <form action="{{ route('admin.RemoveRoleFromUser', ['id' => $user->id]) }}"
+                                                method="post">
+                                                @csrf
+                                                <input type="hidden" name="role" value="{{ $item->name }}">
+                                                <input class="btn btn-danger" type="submit" value="{{ $item->name }}">
+                                            </form>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card mb-4 mb-md-0">
+                                <div class="card-body">
+                                    <p class="mb-4"><span class="text-primary font-italic me-1">permissions
+                                            ({{ $user->permissions->count() }})</span>
+                                    </p>
+                                    <div>
+                                        <form action="{{ route('admin.AssignPermissionToUser', ['id' => $user->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            <select class="form-select" name="permission"
+                                                aria-label="Default select example">
+                                                @foreach ($permissions as $item)
+                                                    <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            <button type="submit" class="btn btn-success">assign permission</button>
+                                        </form>
+                                    </div>
+                                    @foreach ($user->permissions as $item)
+                                        <form action="{{ route('admin.RemovePermissionFromUser', ['id' => $user->id]) }}"
+                                            method="post">
+                                            @csrf
+                                            <input type="hidden" name="role" value="{{ $item->name }}">
+                                            <input class="btn btn-warning" type="submit" value="{{ $item->name }}">
+                                        </form>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </section>
 @endsection
