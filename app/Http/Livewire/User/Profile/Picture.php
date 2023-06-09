becbd9f01cb0ba8daaef674021d2f1568c9c5b79
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-06-09 20:37:40 +0100

    Update Picture.php

--- app/Http/Livewire/User/Profile/Picture.php
+++ app/Http/Livewire/User/Profile/Picture.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Livewire\User\Profile;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Picture extends Component
 {
+    use WithFileUploads;
+
+    public $photo;
     public function render()
     {
         return view('livewire.user.profile.picture');
+    }
+    public function save()
+    {
+        $this->validate([
+            'photo' => 'required',
+        ]);
+
+        if (Auth()->user()->image === "avatar.png") {
+            $filename = $this->photo->store('profiles', 'public');
+            User::find(Auth()->user()->id)->update([
+                'image' => $filename,
+            ]);
+        } else {
+            \Storage::delete(Auth()->user()->image);
+            $filename = $this->photo->store('profiles', 'public');
+            User::find(Auth()->user()->id)->update([
+                'image' => $filename,
+            ]);
+        }
+
+
     }
 }
