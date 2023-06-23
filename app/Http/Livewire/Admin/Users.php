34acb3e37af980ca5f391685949f8873d5e86471
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-06-23 12:47:29 +0100

    add user functions in admin panel

--- app/Http/Livewire/Admin/Users.php
+++ app/Http/Livewire/Admin/Users.php
@@ -3,13 +3,45 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Spatie\Permission\Models\Role;
 
 class Users extends Component
 {
+    public $roles, $name, $email, $password, $role;
     public function render()
     {
         return view('livewire.admin.users');
+    }
+
+    public function mount()
+    {
+        $this->roles = Role::all();
+    }
+
+    public function getRolesProperty()
+    {
+        return $this->roles = Role::all();
+    }
+    public function AddUser()
+    {
+        $this->validate();
+        $user = User::create([
+            "name" => $this->name,
+            "email" => $this->email,
+            "password" => Hash::make($this->password),
+        ]);
+        $user->assignRole($this->role);
+        $user->givePermissionTo(['crud_posts', 'chat', 'comment']);
+        $this->reset();
+    }
+
+    public function clear()
+    {
+        $this->name = "";
+        $this->email = "";
+        $this->password = "";
     }
 
     public function getUsersProperty()
@@ -22,4 +54,9 @@ class Users extends Component
         User::find($id)->delete();
         $this->getUsersProperty();
     }
+    protected $rules = [
+        'name' => 'required|max:200',
+        'email' => 'required|email|max:200',
+        'password' => 'required|min:6|max:200',
+    ];
 }
--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -1,6 +1,6 @@
-<div class="container-fluid" x-data="{ open:false }" wire:poll.750ms>
+<div class="container-fluid" x-data="{ open: false }" wire:poll.750ms>
     <!-- DataTales Example -->
-    <div class="card shadow mb-4" >
+    <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
             <div class="d-flex justify-content-center">
@@ -25,35 +25,35 @@
                     <tbody>
                         @foreach ($this->users as $item)
                             <tr>
-                                <td>{{$item->id}}</td>
-                                <td>{{$item->name}}</td>
-                                <td>{{$item->email}}</td>
-                                <td>{{$item->Posts->count()}}</td>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->email }}</td>
+                                <td>{{ $item->Posts->count() }}</td>
                                 <td>
                                     @if ($item->roles)
                                         @foreach ($item->roles as $role)
                                             <button
-                                            @if ($role->name == "admin")
-                                                class="badge badge-success"
+                                                @if ($role->name == 'admin') class="badge badge-success"
                                             @else
-                                                class="badge badge-primary"
-                                            @endif
-                                            >{{$role->name}}</button>
+                                                class="badge badge-primary" @endif>{{ $role->name }}</button>
                                         @endforeach
                                     @endif
                                 </td>
                                 <td>
                                     {{ $item->permissions->count() }}
                                 </td>
-                                <td class="d-flex">
-                                    @if ($item->posts->count() == 0)
-                                        <button class="btn btn-danger" wire:click="delete({{$item->id}})">
+                                <td>
+                                    {{-- <div class="d-flex"> --}}
+                                        @if ($item->posts->count() == 0)
+                                        <button class="btn btn-danger" wire:click="delete({{ $item->id }})">
                                             <i class="fa-solid fa-trash"></i>
                                         </button>
-                                    @endif
-                                    <a href="{{ route('admin.user.profile', ['id'=>$item->id]) }}" class="btn btn-success">
-                                        <i class="fa-solid fa-eye"></i>
-                                    </a>
+                                        @endif
+                                        <a href="{{ route('admin.user.profile', ['id' => $item->id]) }}"
+                                            class="btn btn-success">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    {{-- </div> --}}
                                 </td>
                             </tr>
                         @endforeach
@@ -61,16 +61,47 @@
                 </table>
                 {{-- {{$this->users->links()}} --}}
             </div>
-            {{-- <div x-show="open" x-transition>
-                <h1 class="text text-center">Add new categorie</h1>
-                <form wire:submit.prevent="add">
+            <div x-show="open" x-transition>
+                <h1 class="text text-center">Add new User</h1>
+                <form wire:submit.prevent="AddUser">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">name :</label>
-                        <input type="text" class="form-control" wire:model="name">
-                        @error('name') <span class="test text-danger">{{ $message }}</span> @enderror
+                        <input type="text" class="form-control" wire:model="name" placeholder="username">
+                        @error('name')
+                            <span class="test text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">email :</label>
+                        <input type="email" class="form-control" wire:model="email" placeholder="email">
+                        @error('email')
+                            <span class="test text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">password :</label>
+                        <input type="text" class="form-control" wire:model="password" placeholder="password">
+                        @error('password')
+                            <span class="test text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Role :</label>
+                        <select class="form-select" aria-label="Default select example" wire:model="role">
+                            <option selected>Open this select menu</option>
+                            @foreach ($this->Roles as $item)
+                                <option value="{{ $item->name }}">{{ $item->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('role')
+                            <span class="test text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary" @click="open = false">Add Acount</button>
                     </div>
                 </form>
-            </div> --}}
+            </div>
         </div>
     </div>
 
